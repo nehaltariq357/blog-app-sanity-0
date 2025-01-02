91a2e7f50a73b7e13c45c03240eb2476dbfbff51
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2025-01-02 14:19:33 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,9 +9,7 @@ import Container from "@/components/Container";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DEVLAB: Discover the Latest in Web Development and AI",
-  description:
-    "Welcome to our web development and AI blog, where we share our insights, opinions, and best practices on web development and AI. Here you will find articles on various topics, such as web design, web development, web security, web performance, AI, machine learning, deep learning, and more. Whether you are a web developer, a web designer, or a web enthusiast, you will find something interesting and valuable on our blog. Check out our latest posts and subscribe to our newsletter to stay updated.",
+  title: "blog-app-sanity",
 };
 
 export default function RootLayout({
