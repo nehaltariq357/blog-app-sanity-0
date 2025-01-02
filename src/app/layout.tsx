6ebc3cc126479508794c1b61ae62c411e0e268cb
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2025-01-02 14:17:40 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,8 @@ const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "DEVLAB: Discover the Latest in Web Development and AI",
-  description: "Welcome to our web development and AI blog, where we share our insights, opinions, and best practices on web development and AI. Here you will find articles on various topics, such as web design, web development, web security, web performance, AI, machine learning, deep learning, and more. Whether you are a web developer, a web designer, or a web enthusiast, you will find something interesting and valuable on our blog. Check out our latest posts and subscribe to our newsletter to stay updated.",
+  description:
+    "Welcome to our web development and AI blog, where we share our insights, opinions, and best practices on web development and AI. Here you will find articles on various topics, such as web design, web development, web security, web performance, AI, machine learning, deep learning, and more. Whether you are a web developer, a web designer, or a web enthusiast, you will find something interesting and valuable on our blog. Check out our latest posts and subscribe to our newsletter to stay updated.",
 };
 
 export default function RootLayout({
@@ -20,20 +21,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.className} bg-light dark:bg-dark `}
-      >
+      <body className={`${montserrat.className} bg-light dark:bg-dark `}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem = {false}
+          enableSystem={false}
           disableTransitionOnChange
         >
-          <Container>
-          <Navbar />
-          {children}
-          </Container>
-          <Footer />
+          <Container>{children}</Container>
         </ThemeProvider>
       </body>
     </html>
